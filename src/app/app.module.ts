191c87feacc6831b4387952211ebc1fa537c9f95
valentiay <valentiay@yandex.ru>
2019-05-06 22:37:25 +0300

    Hints

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import {AppRoutingModule} from './app-routing.module';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {faCheck, faTimes, faPlus, faPencilAlt} from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheck,
+  faTimes,
+  faPlus,
+  faPencilAlt,
+  faInfoCircle,
+  faExclamationTriangle
+} from '@fortawesome/free-solid-svg-icons';
 import {AccountModule} from './account/account.module';
 import {CommonModule} from '@angular/common';
 import {LandingModule} from './landing/landing.module';
-import { ErrorComponent } from './utils/error/error.component';
 
 
 @NgModule({
@@ -32,6 +38,6 @@ import { ErrorComponent } from './utils/error/error.component';
 })
 export class AppModule {
   constructor() {
-    library.add(faCheck, faTimes, faPlus, faPencilAlt);
+    library.add(faCheck, faTimes, faPlus, faPencilAlt, faInfoCircle, faExclamationTriangle);
   }
 }
--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ErrorComponent} from './error/error.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [ErrorComponent],
@@ -8,7 +9,8 @@ import {ErrorComponent} from './error/error.component';
     ErrorComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ]
 })
 export class UtilsModule { }
